f73b7c05a7c517b17fb4b69f8bc18e9e379cbaeb
Ali Butt <aliiftikharbutt@gmail.com>
2025-02-17 14:31:53 +0500

    unit tests added

---
+++ Assets/Editor/Tests/UnitTesting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class UnitTesting
+{
+    [Test]
+    public void testing()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void secondTest()
+    {        
+        Health health = new Health();
+        health.NetworkedHealth++;
+
+        Assert.AreEqual(103, health.NetworkedHealth);
+    }
+}
